587338034ad6ef3070ca536932cfe4679f463241
Alysha <alyshav805@gmail.com>
2019-12-03 23:43:27 -0700

    rename

---
+++ src/com/flightreservationsystem/view/AddFlightJFrameController.java
@@ -0,0 +1,5 @@
+package com.flightreservationsystem.view;
+
+public class AddFlightJFrameController {
+
+}
---
+++ src/com/flightreservationsystem/view/MainGUIJFrameController.java
@@ -0,0 +1,29 @@
+package com.flightreservationsystem.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainGUIJFrameController implements ActionListener{
+
+	private MainGUIJFrameView mainGUIJFrameView;
+	
+	public MainGUIJFrameController() {
+		
+	}
+	
+	public MainGUIJFrameController(MainGUIJFrameView mainGUIJFrameView) {
+		this.mainGUIJFrameView = mainGUIJFrameView;
+		
+		//Creating required action listeners
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
